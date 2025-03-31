222c05e07f1488624e8624ec56dde2be176d75e0
Rayan <kyawminaung343@gmail.com>
2025-03-31 14:08:06 +0630

    client

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg bg-dark " data-bs-theme="dark">
+      <div class="container-fluid ">
+        <a class="navbar-brand " href="#">
+          Hotel Lux
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link "  href="#">
+                Register
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
